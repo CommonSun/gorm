cf9875a96d6327814cdc82fbf155b6a39b23f5a6
Anthony Powles <yogin@heymarket.com>
2020-11-17 16:45:28 -0800

    Add capability to instrument the DB object

---
+++ heymarket.go
@@ -0,0 +1,11 @@
+package gorm
+
+// Instrumenter interface to help instrument queries
+type Instrumenter interface {
+	Instrument(db *DB) *DB
+}
+
+// Instrument allows an instrumenter to manipulate the db object
+func (s *DB) Instrument(i Instrumenter) *DB {
+	return i.Instrument(s.clone()).db
+}
